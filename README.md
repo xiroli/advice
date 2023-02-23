6192fe1325b300777c311a67554347e281b6f0e7
xiroli <davidmazo98@gmail.com>
2023-02-23 01:42:36 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Frontend Mentor - Advice generator app
+# Frontend Mentor - E-commerce product page
 
-![Design preview for the Advice generator app coding challenge](./design/desktop-preview.jpg)
+![Design preview for the E-commerce product page coding challenge](./design/desktop-preview.jpg)
 
 ## Welcome! 👋
 
@@ -8,19 +8,22 @@ Thanks for checking out this front-end coding challenge.
 
 [Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
 
-**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+**To do this challenge, you need a good understanding of HTML, CSS and JavaScript.**
 
 ## The challenge
 
-Your challenge is to build out this advice generator app using the [Advice Slip API](https://api.adviceslip.com) and get it looking as close to the design as possible.
+Your challenge is to build out this e-commerce product page and get it looking as close to the design as possible.
 
 You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
 
 Your users should be able to:
 
-- View the optimal layout for the app depending on their device's screen size
+- View the optimal layout for the site depending on their device's screen size
 - See hover states for all interactive elements on the page
-- Generate a new piece of advice by clicking the dice icon
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
 
 Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
 
--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,251 @@
 <script setup lang="ts">
-  import dice from "../images/icon-dice.svg";
-  import patternSm from "../images/pattern-divider-mobile.svg";
-  import patternLg from "../images/pattern-divider-desktop.svg";
-  import axios from "axios";
-  import { onMounted, ref } from "vue";
-type Advice = {
-    slip: {
-        id: number;
-        advice: string;
-    };
-};
-let advice = $ref("");
-let adviceId = $ref(0);
-
-   async function getAdvice() {
-    const response = await axios.get<Advice>("https://api.adviceslip.com/advice");
-    return response.data
-   }
-onMounted(async () => {
-    const quote: Advice = await getAdvice();
-    adviceId = quote.slip.id;
-    advice = quote.slip.advice;
-});
-
-function newAdvice() {
-    getAdvice().then((quote) => {
-        adviceId= quote.slip.id;
-        advice = quote.slip.advice;
-    });
-}
+ import menuIcon from "../images/icon-menu.svg"
+ import cart from "../images/icon-cart.svg"
+ import avatar from "../images/image-avatar.png" 
+ import iconClose from "../images/icon-close.svg"
+ import ImageCarousel from "./components/ImageCarousel.vue"
+    import Product from "./components/Product.vue"
+    import product1 from "../images/image-product-1.jpg"
+    import deleteIcon from "../images/icon-delete.svg"
+import { onMounted, ref, watch } from "vue"
+    let menu = $ref(false)
+    let dialog = $ref(null)
+    let cartUser = $ref(false)
+    let cartAmount = $ref(0)
+    function dialogs() {
+        menu = !menu
+        if(!dialog.open) {
+            dialog.showModal()
+        } 
+    }
+    onMounted(() => {
+        
+        console.log(dialog)
+        
+        // dialog.open()
+    })
+    function close() {
+        dialog.close()
+    }
+    function addCart(q: number) {
+        cartAmount += q
+    }
+    function removeCart() {
+        cartAmount--
+    }
+    function removeItem() {
+        cartAmount != 0 ? cartAmount-- : cartAmount = 0
+    }
 </script>
 
 <template>
-    <section class="card-container relative w-[350px] lg:w-[580px] h-[380px] pt-[50px] ">
-        <div class="flex flex-col h-full items-center ">
-            <h1 class="text-[28px] font-bold text-white">ADVICE #{{adviceId}}</h1>
-            <div class="paragraph-container px-4 lg:px-16 pt-4 pb-12 mt-4">
-                <p class="paragraph text-2xl lg:text-3xl">"{{advice}}"</p>
+    <dialog class="sidebar relative left-0 w-3/4 h-full" ref="dialog">
+        <div class="sidebar-content flex flex-col absolute top-20 gap-4 left-6 h-full bg-white">
+            <a>Collections</a>
+            <a>Men</a>
+            <a>Woman</a>
+            <a>About</a>
+            <a>Contact</a>
             </div>
-        </div>
-        <img :src="patternSm" class="absolute  right-1/2 translate-x-1/2 bottom-20 lg:hidden" alt="pattern" />
-        <img :src="patternLg" class="absolute  right-1/2 translate-x-1/2 bottom-20 hidden lg:block" alt="pattern" />
-      <div class="btn-container w-[65px] h-[65px] absolute right-1/2 -bottom-8 translate-x-1/2">
-      <button class="btn w-full flex justify-center items-center h-full rounded-full " @click="newAdvice">
-        <img :src="dice" class="" alt="dice" />
-        </button>
+            <button @click="close" class="absolute left-2 top-2 p-4">
+                <img :src="iconClose" alt="menu icon" class="lg:hidden max-h-max" />
+            </button>
+    </dialog>
+    <header class="header flex flex-row gap-2 p-8">
+        <div class="mt-2">
+            <button @click="dialogs">
+        <img :src="menuIcon" alt="menu icon" class="lg:hidden max-h-max" />
+            </button>
+       </div>
+        <nav class="nav flex w-full ">
+            <p class="logo mr-6">sneakers</p>
+            <div class="gap-5 flex-row mt-1 hidden lg:flex">
+            <a class="desktop-nav">Collections</a>
+            <a class="desktop-nav">Men</a>
+            <a class="desktop-nav">Woman</a>
+            <a class="desktop-nav">About</a>
+            <a class="desktop-nav">Contact</a>
+            </div>
+            <div class="flex justify-end w-full gap-4">
+            <div class="relative">
+            <img @click="cartUser = !cartUser" :src="cart" alt="cart icon" class="cart-icon-container h-5 w-5 lg:mt-2 lg:mr-2 relative" />
+            <div class="absolute left-3 -top-1 circle flex justify-center items-center h-5 w-5" v-if="cartAmount > 0">
+                <p class="text-center text-white">{{cartAmount}}</p>
+</div>
+                </div>
+           
+            
+            <img :src="avatar" alt="avatar" class="h-5 w-5 lg:h-10 lg:w-10 avatar " />
+            
+            </div>
+        </nav>
+        
+    </header>
+    <div class="container pt-8 pl-2 pr-6 ">
+    <div class="cart w-full lg:w-1/3 lg:right-0 h-56  mt-2 absolute z-10 bg-white overflow-hidden " v-if="cartUser">
+        <h1 class="text-start cart-text">Cart</h1>
+        <div class="h-[0.5px] mt-5 divider bg-gray-200 w-full" />
+            <p class="empty mt-10" v-if="cartAmount == 0">Your cart is empty.</p>
+
+        <div class="item-container h-full flex flex-col px-4 " v-if="cartAmount > 0">
+              <div class="flex flex-row gap-2">
+                <img class="h-10 h-10 mt-2" :src="product1" />
+              
+                <div class="flex flex-col w-full gap-1">
+
+                    <p class="text-start text-container">Fall Limited Edition Sneakers</p>
+                    <div class="flex flex-row w-full">
+                    <p class="text-start  text-container mr-2">$125.00 x {{cartAmount}}</p>
+                    <h2>${{cartAmount * 125}}.00</h2>
+                    </div>
+                    
+
+                </div>
+                 <div class="trash">
+                    <img @click="removeItem" :src="deleteIcon" alt="delete" class="h-5 w-5 mt-4" />
+                    </div>
+                  </div>
+           
+                    <div class="basis-1">
+
+            <button class="checkout p-4  text-white" v-if="cartAmount > 0">Checkout</button>
+                    </div>
+
+            </div>
+   
+
+         </div>
+
     </div>
+    <main class="flex flex-col lg:flex-row">
+   <section class="image-carousel lg:w-1/2 lg:p-8 ">
+    <ImageCarousel />
+   </section>
+   <section class="product lg:pt-8 lg:w-1/2 lg:mt-16">
+    <Product @add-to-cart="addCart" @remove-from-cart="removeCart" @open-cart="cartUser = !cartUser" />
     </section>
+    </main>
 </template>
 
 <style scoped>
-    .card-container {
-        background: hsl(217, 19%, 24%);
-        border-radius: 20px;
-    }
-    .btn {
-        background: hsl(150, 100%, 66%);
-    }
-    .btn-container {
-        cursor: pointer;
-    }
-    .btn-container .btn:hover {
-        box-shadow: 0 0 40px hsl(150, 100%, 66%);
-    }
-    h1 {
-        font-size: 15px;
-        font-family: Manrope;
-        color: hsl(150, 100%, 66%);
-        letter-spacing: 5.5px;
-    }
-    .paragraph {
-        font-family: Manrope;
-        color: hsl(193, 38%, 86%);
-    }
+.cart-icon-container {
+    cursor: pointer;
+}
+.cart-icon-container:hover {
+}
+.desktop-nav {
+    font-size: 16px;
+    font-family: Kumbh Sans;
+    color: hsl(219, 9%, 45%);
+    position: relative;
+}
+.avatar {
+    cursor: pointer;
+    position: relative;
+}
+.avatar:hover {
+    border-radius: 100%;
+    border: 3px solid hsl(26, 100%, 55%);
+}
+.desktop-nav:hover::after {
+    content: '';
+    position: absolute;
+    bottom: -10px;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    border: 1px solid hsl(26, 100%, 55%);
+}
+h2 {
+    font-family: Kumbh Sans;
+    font-weight: 700;
+    font-size: 16px;
+    color: hsl(220, 13%, 13%);
+}
+.checkout {
+    background: hsl(26, 100%, 55%);
+    border-radius: 10px;
+    width: 100%;
+    margin-top: 20px;
+}
+.circle {
+    background: hsl(26, 100%, 55%);
+    border-radius: 50px;
+}
+.empty, .text-container  {
+    color:  hsl(219, 9%, 45%);
+}
+.cart-text {
+    font-size: 20px;
+    font-family: Kumbh Sans;
+    font-weight: 700;
+    color: hsl(220, 13%, 13%);
+}
+a {
+    font-size: 16px;
+    font-family: Kumbh Sans;
+    font-weight: 700;
+    color: hsl(220, 13%, 13%);
+    cursor: pointer;
+    position: relative;
+    width: 100%;
+    text-align: start;
+}
+a:hover::after {
+    content: "";
+    margin-top: 5px;
+    border: 1px solid black;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 80%;
+}
+    .logo {
+    font-weight: 700;
+    color: black;
+    font-size: 20px;
+    font-family: Kumbh Sans;
+}
+dialog {
+    transition: all 0.3s ease-in-out;
+    max-height: 100vh;
+}
+dialog:modal {
+    position: absolute;
+    margin: 0px;
+    max-width: auto;
+    max-height: auto;
+    user-select: text;
+    visibility: visible;
+    overflow: auto;
+}
+.cart {
+    max-width: 95%;
+}
 </style>
+
+
+### Primary
+
+- Orange: hsl(26, 100%, 55%)
+- Pale orange: hsl(25, 100%, 94%)
+
+### Neutral
+
+- Very dark blue: hsl(220, 13%, 13%)
+- Dark grayish blue: hsl(219, 9%, 45%)
+- Grayish blue: hsl(220, 14%, 75%)
+- Light grayish blue: hsl(223, 64%, 98%)
+- White: hsl(0, 0%, 100%)
+- Black (with 75% opacity for lightbox background): hsl(0, 0%, 0%)
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 16px
+
+### Font
+
+- Family: [Kumbh Sans](https://fonts.google.com/specimen/Kumbh+Sans)
+- Weights: 400, 700
